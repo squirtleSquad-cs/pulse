9c5beb5c32960f609805645db9641d5abf8c9b0d
David <scriptura7773@gmail.com>
2022-06-20 15:37:13 -0700

    First Commit Pulse

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    //where our bundling of react components start
+    main: path.resolve(__dirname, './client/index.ts'),
+  },
+  output: {
+    //name the file 
+    filename: 'bundle.js',
+    //path where we put the file above into
+    //created everytime npm run build 
+    path: path.resolve(__dirname, './build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    hot: true,
+    proxy: {
+      '*': { target: 'http://localhost:3000' },
+      '/api': { target: 'http://localhost:3000' },
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    })
+  ]
+};
