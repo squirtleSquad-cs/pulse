f9e34c18d84312ade090ea1b80a84743f0b85c37
Eric Komatsu <komatsueric@gmail.com>
2022-06-21 11:11:27 -0700

    No SQL db set-up complete

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb");
 const Schema = mongoose.Schema;
+const url = "mongodb+srv://Pulse:Pulse123@cluster0.3oer1.mongodb.net/?retryWrites=true&w=majority"
+
+const client = new MongoClient(url);
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 const userSchema = new Schema({
     id: {type: Number, required: true},
