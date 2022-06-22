496a1f87d9e3accdba423eff2684783e006fa45e
Eric Komatsu <komatsueric@gmail.com>
2022-06-21 19:03:26 -0700

    7:00 pm commit

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,8 +1,56 @@
-const axios = require('axios').default;
+const { Data } = require("../models/nosqlModel");
+const sql = require("../models/sqlModel");
+const bcrypt = require("bcrypt");
+const axios = require("axios").default;
 
 const loginController = {};
 
 loginController.checkLogin = (req, res, next) => {
   const { username, password } = req.body;
-  
-}
+  const loginQuery = `SELECT * FROM USERS WHERE Username = ${username} AND PASSWORD = ${password}`;
+  sql
+    .query(loginQuery)
+    .then((data) => {
+      if (data.rows > 0) {
+        res.locals.user = data.rows[0];
+        return next();
+      }
+      res.locals.fail = "Login failed";
+      return next();
+    })
+    .catch((err) => {
+      console.log(`Issue with checkLogin ${err}`);
+      next(err);
+    });
+};
+
+loginController.createUser = (req, res, next) => {
+  console.log(["inside create user", req.body]);
+  const { username, password } = req.body;
+  // Hash Password
+  bcrypt
+    .hash(password, 2)
+    .then((hash) => {
+      const values = [username, hash];
+      const userQuery = `INSERT INTO users ("username", "password") VALUES ($1, $2)`;
+      sql.query(userQuery, values).then((sqlRes) => {
+        return next();
+      });
+    })
+    .catch((err) => {
+      console.log(`Issue with createUser, ${err}`);
+      return next(err);
+    });
+};
+
+loginController.createMongo = (req, res, next) => {
+  const { username } = req.body;
+  console.log("hello from mongo");
+  Data.create({ id: username }, function (err, user) {
+    console.log("after mongo query");
+    if (err) return next("err in mongo");
+    return next();
+  });
+};
+
+module.exports = loginController;
--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const loginController = require('../controllers/loginController')
 
-router.post('/', loginController.login, (req, res) => {
-  res.status(200).json('login successful')
+router.post('/check', loginController.checkLogin, (req, res) => {
+  res.status(200).json('Login successful');
 })
+
+router.post('/create', loginController.createUser, loginController.createMongo, (req, res) => {
+  return res.status(200).json('User created');
+})
+
+module.exports = router;
